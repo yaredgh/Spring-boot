ae7ba20ba73c453012d4b5192971f28424937fc9
Yaredg <46734446+Yaredg@users.noreply.github.com>
2020-01-13 16:32:13 -0600

    creat initial project

---
+++ src/main/java/edu/mum/recipe/project/recipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package edu.mum.recipe.project.recipe.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = {"","/","/index"})
+    public String getIndex(){
+
+        return "index";
+    }
+}
